bc54546dafc85d2870fb0874709bba520880fdfa
Flying-Fish-1 <90362659+Flying-Fish-1@users.noreply.github.com>
2024-12-14 23:22:41 +0800

    add a normal enemy

---
+++ Assets/Scripts/NormalEnemy/FiniteState/IdleState.cs
@@ -0,0 +1,279 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : IState
+{
+    private normalEnemyFSM manager;
+    private Parameter parameter;
+
+    private float timer;
+
+    public IdleState(normalEnemyFSM manager)
+    {
+        this.manager = manager;
+        parameter = manager.parameter;
+    }
+    
+    public void OnEnter()
+    {
+        parameter.animator.Play("Idle");
+    }
+
+    public void OnUpdate()
+    {
+        timer += Time.deltaTime;
+
+        if (parameter.getHit)
+        {
+            manager.TransitionState(normalEnemyStateType.Hit);
+        }
+        if (parameter.target != null && parameter.target.position.x >= parameter.chasePoints[0].position.x && parameter.target.position.x <= parameter.chasePoints[1].position.x)
+        {
+            manager.TransitionState(normalEnemyStateType.React);
+        }
+        if (timer >= parameter.idleTime)
+        {
+            manager.TransitionState(normalEnemyStateType.Patrol);
+        }
+    }
+
+    public void OnExit()
+    {
+        timer = 0;
+    }
+}
+
+public class PatrolState : IState
+{
+    private normalEnemyFSM manager;
+    private Parameter parameter;
+
+    private int patrolPosition;
+
+    public PatrolState(normalEnemyFSM manager)
+    {
+        this.manager = manager;
+        parameter = manager.parameter;
+    }
+    
+    public void OnEnter()
+    {
+        parameter.animator.Play("Walk");
+    }
+
+    public void OnUpdate()
+    {
+        manager.FlipTo(parameter.patrolPoints[patrolPosition]);
+
+        manager.transform.position = Vector2.MoveTowards(manager.transform.position, parameter.patrolPoints[patrolPosition].position, parameter.moveSpeed * Time.deltaTime);
+
+        if (parameter.getHit)
+        {
+            manager.TransitionState(normalEnemyStateType.Hit);
+        }
+        if (parameter.target != null && parameter.target.position.x >= parameter.chasePoints[0].position.x && parameter.target.position.x <= parameter.chasePoints[1].position.x)
+        {
+            manager.TransitionState(normalEnemyStateType.React);
+        }
+        if (Vector2.Distance(manager.transform.position, parameter.patrolPoints[patrolPosition].position) < 0.5f)
+        {
+            manager.TransitionState(normalEnemyStateType.Idle);
+        }
+    }
+
+    public void OnExit()
+    {
+        patrolPosition++;
+
+        if (patrolPosition >= parameter.patrolPoints.Length)
+        {
+            patrolPosition = 0;
+        }
+    }
+}
+
+public class ChaseState : IState
+{
+    private normalEnemyFSM manager;
+    private Parameter parameter;
+
+    public ChaseState(normalEnemyFSM manager)
+    {
+        this.manager = manager;
+        parameter = manager.parameter;
+    }
+    
+    public void OnEnter()
+    {
+        parameter.animator.Play("Walk");
+    }
+
+    public void OnUpdate()
+    {
+        manager.FlipTo(parameter.target);
+
+        if (parameter.getHit)
+        {
+            manager.TransitionState(normalEnemyStateType.Hit);
+        }
+        if (parameter.target != null)
+        {
+            manager.transform.position = Vector2.MoveTowards(manager.transform.position, parameter.target.position, parameter.chaseSpeed * Time.deltaTime);
+        }
+        else if (parameter.target == null || manager.transform.position.x <= parameter.chasePoints[0].position.x || manager.transform.position.x >= parameter.chasePoints[1].position.x)
+        {
+            manager.TransitionState(normalEnemyStateType.Idle);
+        }
+        else if (Physics2D.OverlapCircle(parameter.attackPoint.position, parameter.attackArea, parameter.targetLayer))
+        {
+            manager.TransitionState(normalEnemyStateType.Attack);
+        }
+    }
+
+    public void OnExit()
+    {
+        
+    }
+}
+
+public class ReactState : IState
+{
+    private normalEnemyFSM manager;
+    private Parameter parameter;
+    private AnimatorStateInfo info;
+
+    public ReactState(normalEnemyFSM manager)
+    {
+        this.manager = manager;
+        parameter = manager.parameter;
+    }
+    
+    public void OnEnter()
+    {
+        parameter.animator.Play("React");
+    }
+
+    public void OnUpdate()
+    {
+        info = parameter.animator.GetCurrentAnimatorStateInfo(0);
+
+        if (parameter.getHit)
+        {
+            manager.TransitionState(normalEnemyStateType.Hit);
+        }
+        if (info.normalizedTime >= 0.95f)
+        {
+            manager.TransitionState(normalEnemyStateType.Chase);
+        }
+    }
+
+    public void OnExit()
+    {
+        
+    }
+}
+
+public class AttackState : IState
+{
+    private normalEnemyFSM manager;
+    private Parameter parameter;
+    private AnimatorStateInfo info;
+
+    public AttackState(normalEnemyFSM manager)
+    {
+        this.manager = manager;
+        parameter = manager.parameter;
+    }
+    
+    public void OnEnter()
+    {
+        parameter.animator.Play("Attack");
+    }
+
+    public void OnUpdate()
+    {
+        info = parameter.animator.GetCurrentAnimatorStateInfo(0);
+
+        if (parameter.getHit)
+        {
+            manager.TransitionState(normalEnemyStateType.Hit);
+        }
+        if (info.normalizedTime >= 0.95f)
+        {
+            manager.TransitionState(normalEnemyStateType.Chase);
+        }
+    }
+
+    public void OnExit()
+    {
+        
+    }
+}
+
+public class HitState : IState
+{
+    private normalEnemyFSM manager;
+    private Parameter parameter;
+    private AnimatorStateInfo info;
+
+    public HitState(normalEnemyFSM manager)
+    {
+        this.manager = manager;
+        parameter = manager.parameter;
+    }
+    
+    public void OnEnter()
+    {
+        parameter.animator.Play("Hit");
+        parameter.health -= 10;
+    }
+
+    public void OnUpdate()
+    {
+        info = parameter.animator.GetCurrentAnimatorStateInfo(0);
+
+        if (parameter.health <= 0)
+        {
+            manager.TransitionState(normalEnemyStateType.Dead);
+        }
+        else
+        {
+            parameter.target = GameObject.FindWithTag("Player").transform;
+
+            manager.TransitionState(normalEnemyStateType.Chase);
+        }
+    }
+
+    public void OnExit()
+    {
+        parameter.getHit = false;
+    }
+}
+
+public class DeadState : IState
+{
+    private normalEnemyFSM manager;
+    private Parameter parameter;
+
+    public DeadState(normalEnemyFSM manager)
+    {
+        this.manager = manager;
+        parameter = manager.parameter;
+    }
+    
+    public void OnEnter()
+    {
+        parameter.animator.Play("Dead");
+    }
+
+    public void OnUpdate()
+    {
+        
+    }
+
+    public void OnExit()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/NormalEnemy/FiniteState/normalEnemyFSM.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum normalEnemyStateType
+{
+    Idle,
+    Patrol,
+    Chase,
+    React,
+    Attack,
+    Hit,
+    Dead
+}
+
+[Serializable]
+public class Parameter
+{
+    public int health;
+    public float moveSpeed;
+    public float chaseSpeed;
+    public float idleTime;
+    public float attackArea;
+    public Transform[] patrolPoints;
+    public Transform[] chasePoints;
+    public Transform target;
+    public Transform attackPoint;
+    public LayerMask targetLayer;
+    public Animator animator;
+    public bool getHit;
+}
+
+public class normalEnemyFSM : MonoBehaviour
+{
+    public Parameter parameter;
+
+    private IState currentState;
+    private Dictionary<normalEnemyStateType, IState> states = new Dictionary<normalEnemyStateType, IState>();
+    
+    void Start()
+    {
+        states.Add(normalEnemyStateType.Idle, new IdleState(this));
+        states.Add(normalEnemyStateType.Patrol, new PatrolState(this));
+        states.Add(normalEnemyStateType.Chase, new ChaseState(this));
+        states.Add(normalEnemyStateType.React, new ReactState(this));
+        states.Add(normalEnemyStateType.Attack, new AttackState(this));
+        states.Add(normalEnemyStateType.Hit, new HitState(this));
+        states.Add(normalEnemyStateType.Dead, new DeadState(this));
+
+        TransitionState(normalEnemyStateType.Idle);
+
+        parameter.animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        currentState.OnUpdate();
+
+        if (false)
+        {
+            parameter.getHit = true;
+        }
+    }
+
+    public void TransitionState(normalEnemyStateType type)
+    {
+        if (currentState != null)
+        {
+            currentState.OnExit();
+        }
+        
+        currentState = states[type];
+        currentState.OnEnter();
+    }
+
+    public void FlipTo(Transform target)
+    {
+        if (target != null)
+        {
+            if (transform.position.x > target.position.x)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else if (transform.position.x < target.position.x)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            parameter.target = other.transform;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            parameter.target = null;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(parameter.attackPoint.position, parameter.attackArea);
+    }
+}
